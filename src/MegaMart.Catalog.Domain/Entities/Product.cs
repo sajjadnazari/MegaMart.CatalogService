91a201e36269c54e3f00e46aaae4ed4cbae095e2
Sajjad nazari <sajjad_spook@yahoo.com>
2026-09-19 00:22:12 +0330

    Add layers folder and Money value object & product entity.

---
+++ src/MegaMart.Catalog.Domain/Entities/Product.cs
@@ -0,0 +1,58 @@
+﻿using MegaMart.Catalog.Domain.ValueObjects;
+
+namespace MegaMart.Catalog.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Money Price { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public List<string> Images { get; private set; }
+
+        // این دیکشنری همون قلب تپنده MongoDB است که ویژگی‌های متغیر رو توش نگه می‌داریم
+        // مثلا: { "RAM": "16GB", "Color": "Black" }
+        public Dictionary<string, string> Attributes { get; private set; }
+
+        // سازنده خالی برای ابزارهای ORM یا سریالایزرهای MongoDB
+        private Product()
+        {
+            Images = new List<string>();
+            Attributes = new Dictionary<string, string>();
+        }
+
+        // متد Factory برای ایجاد محصول جدید با قوانین بیزینسی
+        public static Product Create(string name, string description, Money price, Guid categoryId, Dictionary<string, string> attributes)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("نام محصول نمی‌تواند خالی باشد.");
+
+            return new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Description = description,
+                Price = price,
+                CategoryId = categoryId,
+                Images = new List<string>(),
+                Attributes = attributes ?? new Dictionary<string, string>()
+            };
+        }
+
+        // متدهای تغییر وضعیت (State Mutations)
+        public void UpdatePrice(Money newPrice)
+        {
+            // اینجا می‌توانیم لاجیک بذاریم، مثلا قیمت جدید نباید کمتر از صفر باشه
+            if (newPrice.Amount < 0)
+                throw new ArgumentException("قیمت نمی‌تواند منفی باشد.");
+
+            Price = newPrice;
+        }
+
+        public void AddImage(string imageUrl)
+        {
+            Images.Add(imageUrl);
+        }
+    }
+}
---
+++ src/MegaMart.Catalog.Domain/ValueObjects/Money.cs
@@ -0,0 +1,17 @@
+﻿namespace MegaMart.Catalog.Domain.ValueObjects
+{
+    public record Money(decimal Amount, string Currency)
+    {
+        // یک مقدار پیش‌فرض کاربردی (مثلاً برای ایران با واحد تومان)
+        public static Money Default => new(0, "IRT");
+
+        // متدهایی برای عملیات مالی می‌توانند اینجا اضافه شوند
+        public Money Add(Money addition)
+        {
+            if (Currency != addition.Currency)
+                throw new InvalidOperationException("ارزها باید یکسان باشند.");
+
+            return new Money(Amount + addition.Amount, Currency);
+        }
+    }
+}
